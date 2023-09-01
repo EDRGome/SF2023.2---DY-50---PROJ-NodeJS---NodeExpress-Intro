68078d636d39076c69cbf34287b4ca886ac0ef20
EDRGome <edrgome@gmail.com>
2023-09-01 19:44:39 -0300

    feat(server): Adicionar manipuladores de rota para endpoints root e /cadastrar/:id
    Implemente manipuladores de rota para os seguintes endpoints:
    
    - '/' manipulador de rota retornando 'Olá, Mundo!'
    - Manipulador de rota '/cadastrar/:id' retornando uma mensagem de boas-vindas com o ID do usuário fornecido

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 3003;
 
+app.get("/", (req, res) => {
+  res.send("Olá, Mundo!");
+});
+
+app.get("/cadastrar/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Bem vindo usuário ${id}`);
+});
+
 app.listen(PORT, () => {
   console.log("Servidor rodando!");
 });
